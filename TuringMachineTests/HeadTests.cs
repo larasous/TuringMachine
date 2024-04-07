615d7835bbf2877073212a2fd27e9b91a014611f
larasous <sousalara822@gmail.com>
2024-04-07 17:43:50 -0300

    :recycle: refactor: Rename unit tests

--- TuringMachineTests/HeadTests.cs
+++ TuringMachineTests/HeadTests.cs
@@ -11,15 +11,9 @@ namespace TuringMachineTests
         {
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-
         [Ignore("Not implemented yet")]
         [Test]
-        public void Constructor_ThrowsArgumentNullException_WhenTapeIsNull()
+        public void Constructor_When_TapeIsNull_Then_ReturnThrowsArgumentNullException_()
         {
             // Arrange
             IEnumerable<char> nullTape = null;
@@ -29,7 +23,7 @@ namespace TuringMachineTests
         }
 
         [Test]
-        public void Constructor_ThrowsArgumentNullException_WhenTapeIsEmpty()
+        public void Constructor_When_TapeIsEmpty_Then_ReturnThrowsArgumentNullException_()
         {
             // Arrange
             IEnumerable<char> emptyTape = new List<char>(); // Cria uma fita vazia
@@ -39,5 +33,34 @@ namespace TuringMachineTests
             Assert.AreEqual("Tape must not be empty.", exception.Message); // Verifica se a mensagem da exceção corresponde à mensagem esperada
         }
 
+        [TestCase("0011", 'X', 0, "X011")] // Escrever 'X' na primeira posição da fita vazia resulta em uma fita com um único caractere 'X'
+        [TestCase("01", 'Y', 0, "Y1")] // Escrever 'Y' na posição 0 da fita "01" resulta na fita "Y1"
+        public void Write_When_CorrectIndex_Then_WriteSymbol(string initialTapeString, char symbolToWrite, int writeIndex, string expectedTapeString)
+        {
+            // Arrange
+            IEnumerable<char> initialTape = initialTapeString.ToCharArray();
+            Head head = new Head(HeadDirection.NoMove, initialTape);
+
+            // Act
+            Head newHead = head.Write(symbolToWrite, writeIndex);
+
+            // Assert
+            char[] updatedTape = newHead.Tape.ToArray();
+            string actualTapeString = new string(updatedTape);
+            Assert.AreEqual(expectedTapeString, actualTapeString, $"Esperava '{expectedTapeString}' após escrever '{symbolToWrite}' na posição {writeIndex}");
+        }
+
+        [Test]
+        [TestCase("01", 'X', -1)] // Tentar escrever em um índice negativo deve lançar IndexOutOfRangeException
+        [TestCase("0011", 'X', 10)]  // Tentar escrever em um índice fora dos limites da fita deve lançar IndexOutOfRangeException
+        public void Write_When_IncorrectIndex_Then_ReturnThrowsExceptionForInvalidIndex(string initialTape, char symbolToWrite, int writeIndex)
+        {
+            // Arrange
+            IEnumerable<char> tape = initialTape.ToCharArray();
+            Head head = new Head(HeadDirection.NoMove, tape);
+
+            // Act + Assert
+            Assert.Throws<IndexOutOfRangeException>(() => head.Write(symbolToWrite, writeIndex));
+        }
     }
 }
